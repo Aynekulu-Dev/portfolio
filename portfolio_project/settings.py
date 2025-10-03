6df675186eb7deae673eae5d84ac1ad81dc62811
Aynekulu <Aynekulu4341@gmail.com>
2025-10-03 20:25:37 +0300

    Add psycopg2-binary for PostgreSQL

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -54,10 +54,7 @@ WSGI_APPLICATION = 'portfolio_project.wsgi.application'
 
 # Database (SQLite for simplicity)
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'), conn_max_age=600)
 }
 
 # Password validation
