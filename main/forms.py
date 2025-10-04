f99f134d07f072e204c58dd4f0b9b179f3504406
Aynekulu <Aynekulu4341@gmail.com>
2025-10-04 11:11:38 +0300

    add updated file

--- main/forms.py
+++ main/forms.py
@@ -1,10 +1,7 @@
 from django import forms
 from .models import Contact
-from captcha.fields import CaptchaField
 
 class ContactForm(forms.ModelForm):
-    captcha = CaptchaField()
-
     class Meta:
         model = Contact
         fields = ['name', 'email', 'message']
