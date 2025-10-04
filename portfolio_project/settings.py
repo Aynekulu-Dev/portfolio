dc771ebf0ee0cdef69ffaff3b0cd0350afd79fd3
Aynekulu <Aynekulu4341@gmail.com>
2025-10-04 13:48:15 +0300

    remove aws secrate

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -135,16 +135,4 @@ CAPTCHA_NOISE_FUNCTIONS = ('captcha.helpers.noise_dots',)
 # Silence reCAPTCHA test key warning (remove for production with real keys)
 SILENCED_SYSTEM_CHECKS = ['captcha.recaptcha_test_key_error']
 
-# AWS S3 for Media
-AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME', default='us-east-1')
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-AWS_DEFAULT_ACL = None  # For public files
-AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 
-# Media to S3
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
-# Keep STATIC as is for WhiteNoise
